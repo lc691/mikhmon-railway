1c3b2e9d49ecf431630854e1e1605ff093ad5b66
lc691 <irian.new23@gmail.com>
2025-07-30 19:19:15 +0700

    Allow Railway domain

--- config/config.php
+++ config/config.php
@@ -1,7 +1,5 @@
 <?php
-// Allow localhost dan semua domain *.railway.app
 $host = $_SERVER['HTTP_HOST'] ?? $_SERVER['SERVER_NAME'];
-
 if (!preg_match('/(localhost|127\.0\.0\.1|\.railway\.app)$/', $host)) {
     header("HTTP/1.1 403 Forbidden");
     exit("<h1>403</h1>FORBIDDEN!<br>Sorry Mikhmon doesn't work on ".$host.", please open it at localhost or IP address.");
