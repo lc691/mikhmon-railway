24210cb5bfe3de799e00a87da770b8322e263cc5
lc691 <irian.new23@gmail.com>
2025-07-30 19:07:47 +0700

    Allow Railway domain

--- config/config.php
+++ config/config.php
@@ -1,11 +1,12 @@
+<?php
 // allow Railway domain + localhost
 $allowed_domains = [
-	'localhost',
-	'127.0.0.1',
-	'mikhmon-railway-production.up.railway.app' // domain Railway kamu
+    'localhost',
+    '127.0.0.1',
+    'mikhmon-railway-production.up.railway.app' // <- ganti ini jika domain kamu berbeda
 ];
 
 if (!in_array($_SERVER['SERVER_NAME'], $allowed_domains)) {
-	header("HTTP/1.1 403 Forbidden");
-	exit("<h1>403</h1>FORBIDDEN!<br>Sorry Mikhmon doesn't work on ".$_SERVER['SERVER_NAME'].", please open it at localhost or IP address.");
+    header("HTTP/1.1 403 Forbidden");
+    exit("<h1>403</h1>FORBIDDEN!<br>Sorry Mikhmon doesn't work on ".$_SERVER['SERVER_NAME'].", please open it at localhost or IP address.");
 }
